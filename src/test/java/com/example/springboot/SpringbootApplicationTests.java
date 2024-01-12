197f10bef03d69a1ee3189edf5508c361e4a3fd7
sada2knuy <sada2knuy@naver.com>
2024-01-12 16:52:16 +0900

    CI 추가

--- src/test/java/com/example/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/example/springboot/SpringbootApplicationTests.java
@@ -1,10 +1,8 @@
 package com.example.springboot;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.stream.IntStream;
 
 @SpringBootTest
 class SpringbootApplicationTests {
